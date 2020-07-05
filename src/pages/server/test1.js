19f6600e617b13c859f8a1a7cb1ae0f93037d768
Felipe Gomes <felipe.oliveira@escolamais.com>
2020-07-05 20:39:50 -0300

    .

--- src/pages/server/test1.js
+++ src/pages/server/test1.js
@@ -1,10 +1,11 @@
+const doAsyncWork = () => Promise.reject(new Error('Server Test 1'));
+doAsyncWork();
+
 const Test1 = () => <h1>Server Test 1</h1>;
 
+// Define getServerSideProps so that the page will be server rendered
+// instead of statically generated
 export async function getServerSideProps() {
-  const doAsyncWork = () => Promise.reject(new Error('Server Test 1'));
-
-  doAsyncWork();
-
   return { props: {} };
 }
 
--- src/pages/server/test2.js
+++ src/pages/server/test2.js
@@ -1,10 +1,11 @@
+const doAsyncWork = () => Promise.reject(new Error('Server Test 2'));
+doAsyncWork();
+
 const Test2 = () => <h1>Server Test 2</h1>;
 
+// Define getServerSideProps so that the page will be server rendered
+// instead of statically generated
 export async function getServerSideProps() {
-  const doAsyncWork = () => Promise.reject(new Error('Server Test 2'));
-
-  doAsyncWork();
-
   return { props: {} };
 }
 
